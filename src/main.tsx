c3419aa609c58179c46367e7f11cc18627e86f29
Kone <philipkone45@.com>
2026-05-21 17:39:22 +0000

    feat: implement react-snap for SSG to resolve Search Console 404 indexing errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,32 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { hydrateRoot, createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { CurrencyProvider } from './context/CurrencyContext.tsx'
 import ErrorBoundary from './components/ErrorBoundary.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <CurrencyProvider>
-        <App />
-      </CurrencyProvider>
-    </ErrorBoundary>
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root')!;
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(
+    rootElement,
+    <React.StrictMode>
+      <ErrorBoundary>
+        <CurrencyProvider>
+          <App />
+        </CurrencyProvider>
+      </ErrorBoundary>
+    </React.StrictMode>
+  );
+} else {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <ErrorBoundary>
+        <CurrencyProvider>
+          <App />
+        </CurrencyProvider>
+      </ErrorBoundary>
+    </React.StrictMode>
+  );
+}
